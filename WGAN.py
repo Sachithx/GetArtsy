8c4f89fb205861e3e1e3cd7cb111ac53abf26061
Sachith <sachith.abeywickrama@rootcodelabs.com>
2022-11-24 16:55:25 +0530

    WGANs

--- WGAN.py
+++ WGAN.py
@@ -41,3 +41,61 @@ class Generator(nn.Module):
     def forward(self, noise):
         noise = noise.view(noise.size(0), noise.size(1), 1, 1)
         return self.gen(noise)
+
+def get_noise(n_samples, z_dim, device='cpu'):
+    return torch.randn(n_samples, z_dim, device=device)
+
+class Critic(nn.Module):
+    def __init__(self, image_channel=3):
+        super(Critic, self).__init__()
+        self.disc = nn.Sequential(
+       
+            nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2, inplace=True),
+   
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+ 
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+      
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+ 
+            nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False),
+        )
+        
+    def forward(self, image):
+        crit_pred = self.disc(image)
+        return crit_pred.view(len(crit_pred), -1)
+
+n_epochs = 100
+z_dim = 64
+display_step = 50
+batch_size = 128
+lr = 0.0002
+beta_1 = 0.5
+beta_2 = 0.999
+c_lambda = 10
+crit_repeats = 5
+device = 'cpu'
+
+image_size = (128,128)
+stats = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
+path = '/Users/Sachith/dataset/Data'
+
+transform = transforms.Compose([transforms.Resize(image_size),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize(*stats)
+                                   ])
+
+data = torchvision.datasets.ImageFolder(root=path,
+                                     transform=transform)
+
+dataloader = DataLoader(data, batch_size, shuffle=True, num_workers=3, pin_memory=True)
+print(len(data))
+
