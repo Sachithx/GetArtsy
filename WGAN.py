252ee0050b25cb8acf0af3b5d946f9880cbc5eba
Sachith <117737754+Sachithx@users.noreply.github.com>
2022-11-24 17:01:39 +0530

    Add files via upload

--- WGAN.py
+++ WGAN.py
@@ -41,5 +41,115 @@ class Generator(nn.Module):
     def forward(self, noise):
         noise = noise.view(noise.size(0), noise.size(1), 1, 1)
         return self.gen(noise)
-    
-    print("Demo")
+
+def get_noise(n_samples, z_dim, device='cpu'):
+    return torch.randn(n_samples, z_dim, device=device)
+
+class Critic(nn.Module):
+    def __init__(self, image_channel=3):
+        super(Critic, self).__init__()
+        self.disc = nn.Sequential(
+       
+            nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2, inplace=True),
+   
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+ 
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+      
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+ 
+            nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False),
+        )
+        
+    def forward(self, image):
+        crit_pred = self.disc(image)
+        return crit_pred.view(len(crit_pred), -1)
+
+n_epochs = 100
+z_dim = 64
+display_step = 50
+batch_size = 128
+lr = 0.0002
+beta_1 = 0.5
+beta_2 = 0.999
+c_lambda = 10
+crit_repeats = 5
+device = 'cpu'
+
+image_size = (128,128)
+stats = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
+path = '/Users/Sachith/dataset/Data'
+
+transform = transforms.Compose([transforms.Resize(image_size),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize(*stats)
+                                   ])
+
+data = torchvision.datasets.ImageFolder(root=path,
+                                     transform=transform)
+
+dataloader = DataLoader(data, batch_size, shuffle=True, num_workers=3, pin_memory=True)
+print(len(data))
+
+gen = Generator(z_dim).to(device)
+gen_opt = torch.optim.Adam(gen.parameters(), lr=lr, betas=(beta_1, beta_2))
+crit = Critic().to(device) 
+crit_opt = torch.optim.Adam(crit.parameters(), lr=lr, betas=(beta_1, beta_2))
+
+def weights_init(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+    if isinstance(m, nn.BatchNorm2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+        torch.nn.init.constant_(m.bias, 0)
+gen = gen.apply(weights_init)
+crit = crit.apply(weights_init)
+
+def get_gradient(crit, real, fake, epsilon):
+    mixed_images = real * epsilon + fake * (1 - epsilon)
+    mixed_scores = crit(mixed_images)
+    gradient = torch.autograd.grad(
+        inputs=mixed_images,
+        outputs=mixed_scores,
+        grad_outputs=torch.ones_like(mixed_scores), 
+        create_graph=True,
+        retain_graph=True,
+    )[0]
+    return gradient
+
+def gradient_penalty(gradient):
+    gradient = gradient.view(len(gradient), -1)
+    gradient_norm = gradient.norm(2, dim=1)
+    penalty = torch.mean((gradient_norm - 1)**2)
+    return penalty
+
+def get_gen_loss(crit_fake_pred):
+    gen_loss = -1. * torch.mean(crit_fake_pred)
+    return gen_loss
+
+def get_crit_loss(crit_fake_pred, crit_real_pred, gp, c_lambda):
+    crit_loss = torch.mean(crit_fake_pred) - torch.mean(crit_real_pred) + c_lambda * gp
+    return crit_loss
+
+def show_tensor_images(image_tensor, num_images=25, size=(3, 64, 64)):
+    image_tensor = (image_tensor + 1) / 2
+    image_unflat = image_tensor.detach().cpu()
+    image_grid = make_grid(image_unflat[:num_images], nrow=5)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    plt.show()
+
+def make_grad_hook():
+    grads = []
+    def grad_hook(m):
+        if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+            grads.append(m.weight.grad)
+    return grads, grad_hook
+
