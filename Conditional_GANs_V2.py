0a0849e0f9c4a8203c6ee4af9154b961d6bb1fca
Sachith <sachith.abeywickrama@rootcodelabs.com>
2022-12-12 12:15:12 +0530

    Conditional GANs Version II

---
+++ Conditional_GANs_V2.py
@@ -0,0 +1,257 @@
+## Import Libraries and Packages
+import torch
+import torchvision
+from torch import nn
+from torchvision import transforms
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+torch.manual_seed(0)
+
+## Visualize Images
+def show_tensor_images(image_tensor, num_images=25, size=(3, 128, 128), nrow=5, show=True):
+    image_tensor = (image_tensor + 1) / 2
+    image_unflat = image_tensor.detach().cpu()
+    image_grid = make_grid(image_unflat[:num_images], nrow=nrow)
+    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+    if show:
+        plt.show()
+
+## Build the Generator
+class Generator(nn.Module):
+    def __init__(self, noise_dim, image_channel=3):
+        super(Generator, self).__init__()
+        self.gen = nn.Sequential(
+
+            nn.ConvTranspose2d(noise_dim, 1024, kernel_size=4, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(1024),
+            nn.ReLU(inplace=True),
+
+            nn.ConvTranspose2d(1024, 512, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+
+            nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+
+            nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+      
+            nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+
+            nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.Tanh(),
+        )
+        
+    def forward(self, noise):
+        noise = noise.view(noise.size(0), noise.size(1), 1, 1)
+        return self.gen(noise)
+
+## Create Random Noise
+def get_noise(n_samples, input_dim, device='cpu'):
+    return torch.randn(n_samples, input_dim, device=device)
+
+## Build Critic
+class Critic(nn.Module):
+    def __init__(self):
+        super(Critic, self).__init__()
+        self.disc = nn.Sequential(
+       
+            nn.Conv2d(13, 64, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(64),
+            nn.LeakyReLU(0.2, inplace=True),
+   
+            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(128),
+            nn.LeakyReLU(0.2, inplace=True),
+ 
+            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.LeakyReLU(0.2, inplace=True),
+      
+            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.BatchNorm2d(512),
+            nn.LeakyReLU(0.2, inplace=True),
+ 
+            nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False),
+        )
+        
+    def forward(self, image):
+        crit_pred = self.disc(image)
+        return crit_pred.view(len(crit_pred), -1)
+
+## Get One-Hot Labels for Artist Names
+import torch.nn.functional as F
+def get_one_hot_labels(labels, n_classes):
+    return F.one_hot(labels, n_classes)
+
+## Combine vectors
+def combine_vectors(x, y):
+    combined = torch.cat((x.float(), y.float()), 1)
+    return combined
+
+## Training Loop
+arts_shape = (3, 128, 128)
+n_classes = 10
+n_epochs = 100
+z_dim = 64
+display_step = 500
+batch_size = 128
+lr = 0.0002
+device = 'cuda'
+image_size = (128,128)
+stats = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
+crit_repeats = 5
+c_lambda = 10
+transform = transforms.Compose([transforms.Resize(image_size),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize(*stats)
+                                   ])
+
+data = torchvision.datasets.ImageFolder(root='/kaggle/input/cond-arts',
+                                     transform=transform)
+
+dataloader = DataLoader(data, batch_size, shuffle=True, num_workers=2, pin_memory=True)
+print(len(data))
+
+def get_input_dimensions(z_dim, arts_shape, n_classes):
+    generator_input_dim = z_dim + n_classes
+    discriminator_im_chan = arts_shape[0] + n_classes
+    return generator_input_dim, discriminator_im_chan
+
+generator_input_dim, _ = get_input_dimensions(z_dim, arts_shape, n_classes)
+
+gen = Generator(noise_dim=generator_input_dim).to(device)
+gen_opt = torch.optim.Adam(gen.parameters(), lr=lr)
+crit = Critic().to(device)
+crit_opt = torch.optim.Adam(crit.parameters(), lr=lr)
+
+def weights_init(m):
+    if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+    if isinstance(m, nn.BatchNorm2d):
+        torch.nn.init.normal_(m.weight, 0.0, 0.02)
+        torch.nn.init.constant_(m.bias, 0)
+gen = gen.apply(weights_init)
+crit = crit.apply(weights_init)
+
+## Create Gradient
+def get_gradient(crit, real, fake, epsilon):
+    mixed_images = real * epsilon + fake * (1 - epsilon)
+    mixed_scores = crit(mixed_images)
+    gradient = torch.autograd.grad(
+        inputs=mixed_images,
+        outputs=mixed_scores,
+        grad_outputs=torch.ones_like(mixed_scores), 
+        create_graph=True,
+        retain_graph=True,
+    )[0]
+    return gradient
+
+## Define Gradient Penalty
+def gradient_penalty(gradient):
+    gradient = gradient.view(len(gradient), -1)
+    gradient_norm = gradient.norm(2, dim=1)
+    penalty = torch.mean((gradient_norm - 1)**2)
+    return penalty
+
+## Generator Loss
+def get_gen_loss(crit_fake_pred):
+    gen_loss = -1. * torch.mean(crit_fake_pred)
+    return gen_loss
+
+## Critic Loss
+def get_crit_loss(crit_fake_pred, crit_real_pred, gp, c_lambda):
+    crit_loss = torch.mean(crit_fake_pred) - torch.mean(crit_real_pred) + c_lambda * gp
+    return crit_loss
+
+
+cur_step = 0
+generator_losses = []
+critic_losses = []
+
+for epoch in range(n_epochs):
+    print(epoch)
+    # Dataloader returns the batches and the labels
+    for real, labels in dataloader:
+        cur_batch_size = len(real)
+        # Flatten the batch of real images from the dataset
+        real = real.to(device)
+        one_hot_labels = get_one_hot_labels(labels.to(device), n_classes)
+        image_one_hot_labels = one_hot_labels[:, :, None, None]
+        image_one_hot_labels = image_one_hot_labels.repeat(1, 1, arts_shape[1], arts_shape[2])
+        
+        mean_iteration_critic_loss = 0
+        for _ in range(crit_repeats):
+            ### Update Critic
+            crit_opt.zero_grad()
+            # Get noise corresponding to the current batch_size 
+            fake_noise = get_noise(cur_batch_size, z_dim, device=device)
+        
+            noise_and_labels = combine_vectors(fake_noise, one_hot_labels)
+            fake = gen(noise_and_labels)
+
+            fake_image_and_labels = combine_vectors(fake, image_one_hot_labels)
+            real_image_and_labels = combine_vectors(real, image_one_hot_labels)
+            crit_fake_pred = crit(fake_image_and_labels.detach())
+            crit_real_pred = crit(real_image_and_labels)
+            
+            
+            epsilon = torch.rand(len(real), 1, 1, 1, device=device, requires_grad=True)
+            gradient = get_gradient(crit, real_image_and_labels, fake_image_and_labels.detach(), epsilon)
+            gp = gradient_penalty(gradient)
+            crit_loss = get_crit_loss(crit_fake_pred, crit_real_pred, gp, c_lambda)
+
+            # Keep track of the average critic loss in this batch
+            mean_iteration_critic_loss += crit_loss.item() / crit_repeats
+            # Update gradients
+            crit_loss.backward(retain_graph=True)
+            # Update optimizer
+            crit_opt.step()
+
+        # Keep track of the average discriminator loss
+        critic_losses += [crit_loss.item()]
+
+        ### Update generator ###
+        # Zero out the generator gradients
+        gen_opt.zero_grad()
+
+        fake_image_and_labels = combine_vectors(fake, image_one_hot_labels)
+        # This will error if you didn't concatenate your labels to your image correctly
+        crit_fake_pred = crit(fake_image_and_labels)
+        gen_loss = get_gen_loss(crit_fake_pred)
+        gen_loss.backward()
+        gen_opt.step()
+        
+        # Keep track of the generator losses
+        generator_losses += [gen_loss.item()]
+        #
+
+        if cur_step % display_step == 0 and cur_step > 0:
+            gen_mean = sum(generator_losses[-display_step:]) / display_step
+            crit_mean = sum(critic_losses[-display_step:]) / display_step
+            print(f"Step {cur_step}: Generator loss: {gen_mean}, critic loss: {crit_mean}")
+            show_tensor_images(fake)
+            show_tensor_images(real)
+            step_bins = 20
+            x_axis = sorted([i * step_bins for i in range(len(generator_losses) // step_bins)] * step_bins)
+            num_examples = (len(generator_losses) // step_bins) * step_bins
+            plt.plot(
+                range(num_examples // step_bins), 
+                torch.Tensor(generator_losses[:num_examples]).view(-1, step_bins).mean(1),
+                label="Generator Loss"
+            )
+            plt.plot(
+                range(num_examples // step_bins), 
+                torch.Tensor(critic_losses[:num_examples]).view(-1, step_bins).mean(1),
+                label="Critic Loss"
+            )
+            plt.legend()
+            plt.show()
+        elif cur_step == 0:
+            print("Works for one complete epoch!!")
+        cur_step += 1
