2baab8708b0641c7a5550a47ac3b079e89d95161
Sachithx <sachith.abeywickrama@rootcodelabs.com>
2023-02-09 17:22:24 +0530

    changes

--- Conditional_GANs_V1.py
+++ Conditional_GANs_V1.py
@@ -1,4 +1,4 @@
-## Import Libraries and Packages
+# Import Libraries and Packages
 import os
 import sys
 import tensorflow as tf
@@ -9,33 +9,34 @@ import numpy as np
 from numpy import zeros, ones
 from numpy.random import randn, randint
 import cv2
-from keras.layers import Dense, Reshape, Flatten, LeakyReLU, Conv2DTranspose, Conv2D, Dropout, Embedding, Concatenate, Input
+from keras.layers import Dense, Reshape, Flatten, LeakyReLU, Conv2DTranspose, Conv2D, Dropout, Embedding, Concatenate
+from keras.layers import Input
 from keras.models import Model
 from keras.optimizers import Adam
 from sklearn import preprocessing
 from numpy import asarray
 from keras.models import load_model
 
-## Versions 
+# Versions
 print(f"Python Platform: {platform.platform()}")
 print(f"Tensor Flow Version: {tf.__version__}")
 print(f"Keras Version: {keras.__version__}")
 print(f"Python {sys.version}")
 
-## Set Artists Names for 10 Artists, with 100 Arts per each
+# Set Artists Names for 10 Artists, with 100 Arts per each
 list = ['Alfred_Sisley',
-'Amedeo_Modigliani', 
-'Gustav_Klimt',
-'Marc_Chagall',
-'Pablo_Picasso',
-'Paul_Klee',
-'Peter_Paul_Rubens',
-'Pieter_Bruegel',
-'Raphael',
-'Rembrandt']
+        'Amedeo_Modigliani',
+        'Gustav_Klimt',
+        'Marc_Chagall',
+        'Pablo_Picasso',
+        'Paul_Klee',
+        'Peter_Paul_Rubens',
+        'Pieter_Bruegel',
+        'Raphael',
+        'Rembrandt']
 print(f"Number of Artists: {len(list)}")
 
-## Import dataset with Arts and Artist Names
+# Import dataset with Arts and Artist Names
 print("Importing the Dataset...")
 directory = '/Users/Sachith/dataset/Arts'
 categories = list
@@ -45,7 +46,7 @@ categories = categories
 
 data = []
 labels = []
-        
+
 for category in categories:
     path = os.path.join(directory, category)
     for img in os.listdir(path):
@@ -53,7 +54,7 @@ for category in categories:
         image = cv2.imread(img_path)
         data.append(image)
         labels.append(category)
-        
+
 n_arts = f"Number of Arts: {len(data)} loaded"
 n_artists = f"Number of Artists: {len(list)}"
 
@@ -61,8 +62,8 @@ print(n_arts)
 print(n_artists)
 print("Loading Completed!!")
 
-## Resize to Similar Size
-## Set the image dimensions to resize
+# Resize to Similar Size
+# Set the image dimensions to resize
 width = 128
 height = 128
 
@@ -72,51 +73,54 @@ dim = (width, height)
 resized_images = []
 
 for image in data:
-    resized = cv2.resize(image, dim, interpolation = cv2.INTER_AREA)
+    resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
     resized_images.append(resized)
 
 arts = np.array(resized_images)
-    
+
 print(f"Resizing Completed!! New size is {width}x{height}")
 artist_names = np.array(labels)
 
-## Assign some useful parameters
+# Assign some useful parameters
 image_shape = arts[0].shape
 print(f"Image shape is: {image_shape}")
 
-## Label Encoding
+# Label Encoding
 le = preprocessing.LabelEncoder()
 le.fit(artist_names)
 artist_names = le.transform(artist_names).astype('uint8')
 
-### Load function for training the model (Returning the art and artist name)
+
+# Load function for training the model (Returning the art and artist name)
 def load_arts(arts=arts, artist_names=artist_names):
-    arts = arts.astype('float32')  ## Convert to floats
-    arts = arts / 255 ## Rescale
-    artist_names = artist_names.reshape(-1,1)
+    arts = arts.astype('float32')  # Convert to floats
+    arts = arts / 255  # Rescale
+    artist_names = artist_names.reshape(-1, 1)
     return [arts, artist_names]
 
+
 ## Build the Discriminator
-def build_discriminator(input_shape=(128,128,3), n_artists=10):
+def build_discriminator(input_shape=(128, 128, 3), n_artists=10):
     ## Considering the labels
-    label = Input(shape=(1,)) ## Label input
+    label = Input(shape=(1,))  ## Label input
     ## Each artist will be represented by a vector of size 50
     label_input = Embedding(n_artists, 50)(label)
     ## Image dimensions
-    dim_in = input_shape[0] * input_shape[1] ## 128x128
+    dim_in = input_shape[0] * input_shape[1]  ## 128x128
     label_input = Dense(dim_in)(label_input)
     ## Reshape
-    label_input = Reshape((input_shape[0], input_shape[1], 1))(label_input) ## 128x128x1 This is the label input (As a channel)
+    label_input = Reshape((input_shape[0], input_shape[1], 1))(
+        label_input)  ## 128x128x1 This is the label input (As a channel)
 
-    ## Image 
-    input_image = Input(shape=input_shape) ## 128x128x3
+    ## Image
+    input_image = Input(shape=input_shape)  ## 128x128x3
     ## Add the label as a new channel
-    merge = Concatenate()([input_image, label_input]) ## 128x128x4 (3 for image and 1 for label)
+    merge = Concatenate()([input_image, label_input])  ## 128x128x4 (3 for image and 1 for label)
 
-    down = Conv2D(128, (3,3), strides=(2,2), padding='same')(merge) ## 64x64x128
+    down = Conv2D(128, (3, 3), strides=(2, 2), padding='same')(merge)  ## 64x64x128
     down - LeakyReLU(alpha=0.2)(down)
 
-    down = Conv2D(128, (3,3), strides=(2,2), padding='same')(down) ## 32x32x128
+    down = Conv2D(128, (3, 3), strides=(2, 2), padding='same')(down)  ## 32x32x128
     down - LeakyReLU(alpha=0.2)(down)
 
     down = Flatten()(down)
@@ -133,9 +137,11 @@ def build_discriminator(input_shape=(128,128,3), n_artists=10):
 
     return model
 
+
 ## Testing the discriminator
 test_discriminator = build_discriminator()
 print(test_discriminator.summary())
+
 
 def build_generator(noise_dim=100, n_artists=10):
     label = Input(shape=(1,))
@@ -150,36 +156,37 @@ def build_generator(noise_dim=100, n_artists=10):
     input_noise = Input(shape=(noise_dim,))  ## Input dimension: 100
 
     dim_in = 32 * 32 * 128
-    generator = Dense(dim_in)(input_noise) ## 131072
+    generator = Dense(dim_in)(input_noise)  ## 131072
     generator = LeakyReLU(alpha=0.2)(generator)
-    generator = Reshape((32, 32, 128))(generator) ## 32x32x128
+    generator = Reshape((32, 32, 128))(generator)  ## 32x32x128
 
-    merge = Concatenate()([generator, label_input]) ## 32x32x(128+1)
+    merge = Concatenate()([generator, label_input])  ## 32x32x(128+1)
 
-    generator = Conv2DTranspose(128, (4,4), strides=(2,2), padding='same')(merge)
-    generator = LeakyReLU(alpha=0.2)(generator)
-
-    generator = Conv2DTranspose(128, (4,4), strides=(2,2), padding='same')(generator)
+    generator = Conv2DTranspose(128, (4, 4), strides=(2, 2), padding='same')(merge)
     generator = LeakyReLU(alpha=0.2)(generator)
 
-    output_layer = Conv2D(3, (8,8), activation='sigmoid', padding='same')(generator)
+    generator = Conv2DTranspose(128, (4, 4), strides=(2, 2), padding='same')(generator)
+    generator = LeakyReLU(alpha=0.2)(generator)
+
+    output_layer = Conv2D(3, (8, 8), activation='sigmoid', padding='same')(generator)
 
     model = Model([input_noise, label], output_layer)
 
     return model
+
 
 ## Testing the generator
 test_generator = build_generator()
 print(test_generator.summary())
 print(f"The output image shape is {test_generator.output_shape}")
 
+
 ## Combine the GAN model
 def build_gan(discriminator, generator):
-
-    discriminator.trainable = False ## Since we train the dicsriinator separately
+    discriminator.trainable = False  ## Since we train the dicsriinator separately
 
     ## Connect
-    generated_noise, generated_label = generator.input 
+    generated_noise, generated_label = generator.input
     generated_output = generator.output  ## 128x128x3
 
     ## Go through the discriminator
@@ -191,18 +198,20 @@ def build_gan(discriminator, generator):
 
     return model
 
+
 ## Helper Functions for Training Loop
 
 def real_samples(arts_artists, n_samples):
-	# split into images and labels
-	images, labels = arts_artists  
-	# choose random instances
-	ix = randint(0, images.shape[0], n_samples)
-	# select images and labels
-	X, labels = images[ix], labels[ix]
-	# generate class labels and assign to y (don't confuse this with the above labels that correspond to cifar labels)
-	y = ones((n_samples, 1))  #Label=1 indicating they are real
-	return [X, labels], y
+    # split into images and labels
+    images, labels = arts_artists
+    # choose random instances
+    ix = randint(0, images.shape[0], n_samples)
+    # select images and labels
+    X, labels = images[ix], labels[ix]
+    # generate class labels and assign to y (don't confuse this with the above labels that correspond to cifar labels)
+    y = ones((n_samples, 1))  # Label=1 indicating they are real
+    return [X, labels], y
+
 
 def random_noise(noise_dim, samples, n_artists=10):
     generator_input = randn(noise_dim * samples)
@@ -212,13 +221,16 @@ def random_noise(noise_dim, samples, n_artists=10):
     artists_labels = randint(0, n_artists, samples)
     return [generator_input, artists_labels]
 
+
 def fake_samples(generator, noise_dim, samples):
     generator_input, artists = random_noise(noise_dim, samples)
     arts = generator.predict([generator_input, artists])
-    y = zeros((samples, 1)) ## Since the fake arts
+    y = zeros((samples, 1))  ## Since the fake arts
     return [arts, artists], y
 
-def train_the_GAN(generator_model, discriminator_model, gan_model, arts_artists, noise_dim=100, epochs=100, batch_size=64):
+
+def train_the_GAN(generator_model, discriminator_model, gan_model, arts_artists, noise_dim=100, epochs=100,
+                  batch_size=64):
     batches_per_epoch = int(arts_artists[0].shape[0] / batch_size)
     half_batch = int(batch_size / 2)
 
@@ -231,7 +243,7 @@ def train_the_GAN(generator_model, discriminator_model, gan_model, arts_artists,
         generator_loss_acc = 0.
 
         for batch_per_epoch in range(batches_per_epoch):
-            print(f"epoch: {epoch+1} ----- {batch_per_epoch+1}/{batches_per_epoch}")
+            print(f"epoch: {epoch + 1} ----- {batch_per_epoch + 1}/{batches_per_epoch}")
 
             [real_arts, real_artists], real_labels = real_samples(arts_artists, half_batch)
             discriminator_loss_real, _ = discriminator_model.train_on_batch([real_arts, real_artists], real_labels)
@@ -252,25 +264,26 @@ def train_the_GAN(generator_model, discriminator_model, gan_model, arts_artists,
         epoch_loss_generator.append(generator_loss_acc / batches_per_epoch)
 
     ## Save the Generator
-    generator_model.save('Rootcode_conditional.h5') 
+    generator_model.save('Rootcode_conditional.h5')
 
     ## Plot the loss
     x = []
     for i in range(epochs):
-      x.append(i+1)
-      i = i + 1
+        x.append(i + 1)
+        i = i + 1
     y1points = np.array(epoch_loss_discriminator)
     y2points = np.array(epoch_loss_generator)
     xpoints = np.array(x)
-    plt.plot(xpoints, y1points,xpoints, y2points)
+    plt.plot(xpoints, y1points, xpoints, y2points)
     plt.show()
+
 
 ## Define Training Parameters
 generator = build_generator()
 discriminator = build_discriminator()
 gan = build_gan(discriminator, generator)
 arts_artists = load_arts()
-noise_dim =100
+noise_dim = 100
 epochs = 200
 batch_size = 64
 
@@ -284,17 +297,20 @@ noise, labels = random_noise(100, 100)
 # specify labels - generate 10 sets of labels each gping from 0 to 9
 labels = asarray([x for i in range(10) for x in range(10)])
 # generate images
-art  = model.predict([noise, labels])
+art = model.predict([noise, labels])
 ## Rescale to 0-256
 art = (art * 255).astype(np.uint8)
+
+
 # plot the result (10 sets of images, all images in a column should be of same class in the plot)
 # Plot generated images 
 def show_plot(examples, n):
-	for i in range(n * n):
-		plt.subplot(n, n, 1 + i)
-		plt.axis('off')
-		plt.imshow(examples[i, :, :, :])
-	plt.show()
+    for i in range(n * n):
+        plt.subplot(n, n, 1 + i)
+        plt.axis('off')
+        plt.imshow(examples[i, :, :, :])
+    plt.show()
+
 
 show_plot(art, 10)
 
--- WGAN.py
+++ WGAN.py
@@ -1,17 +1,20 @@
-## Import required packaged and libraries
-import torch
-from torch import nn
-from tqdm.auto import tqdm
-from torchvision import transforms
-from torchvision.datasets import MNIST
-from torchvision.utils import make_grid
-from torch.utils.data import DataLoader
-import matplotlib.pyplot as plt
-torch.manual_seed(0)
+# Import required packaged and libraries
 import torchvision
 import os
 
-## Build the Generator
+import torch
+from torch import nn
+from tqdm.auto import tqdm
+from torchvision.utils import save_image
+from torchvision import transforms
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+
+torch.manual_seed(0)
+
+
+# Build the Generator
 class Generator(nn.Module):
     def __init__(self, noise_dim, image_channel=3):
         super(Generator, self).__init__()
@@ -32,7 +35,7 @@ class Generator(nn.Module):
             nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
-      
+
             nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
@@ -40,45 +43,48 @@ class Generator(nn.Module):
             nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1, bias=False),
             nn.Tanh(),
         )
-        
+
     def forward(self, noise):
         noise = noise.view(noise.size(0), noise.size(1), 1, 1)
         return self.gen(noise)
 
-## Create Random Noise
+
+# Create Random Noise
 def get_noise(n_samples, z_dim, device='cuda'):
     return torch.randn(n_samples, z_dim, device=device)
 
-## Build the Critic (Known as the Discriminator in Traditional GANs)
+
+# Build the Critic (Known as the Discriminator in Traditional GANs)
 class Critic(nn.Module):
     def __init__(self):
         super(Critic, self).__init__()
         self.disc = nn.Sequential(
-       
+
             nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(64),
             nn.LeakyReLU(0.2, inplace=True),
-   
+
             nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True),
- 
+
             nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2, inplace=True),
-      
+
             nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(512),
             nn.LeakyReLU(0.2, inplace=True),
- 
+
             nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False),
         )
-        
+
     def forward(self, image):
         crit_pred = self.disc(image)
         return crit_pred.view(len(crit_pred), -1)
 
-## Define training parameters
+
+# Define training parameters
 n_epochs = 600
 z_dim = 64
 display_step = 50
@@ -90,69 +96,77 @@ c_lambda = 10
 crit_repeats = 5
 device = 'cuda'
 
-## Import dataset and dataloader
-image_size = (128,128)
+# Import dataset and dataloader
+image_size = (128, 128)
 stats = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
 path = '/Users/Sachith/dataset/Data'
 
 transform = transforms.Compose([transforms.Resize(image_size),
-                                   transforms.ToTensor(),
-                                   transforms.Normalize(*stats)
-                                   ])
+                                transforms.ToTensor(),
+                                transforms.Normalize(*stats)
+                                ])
 
 data = torchvision.datasets.ImageFolder(root=path,
-                                     transform=transform)
+                                        transform=transform)
 
 dataloader = DataLoader(data, batch_size, shuffle=True, num_workers=3, pin_memory=True)
 print(len(data))
 
-## Initialise Generator and Critic
+# Initialise Generator and Critic
 gen = Generator(z_dim).to(device)
 gen_opt = torch.optim.Adam(gen.parameters(), lr=lr, betas=(beta_1, beta_2))
-crit = Critic().to(device) 
+crit = Critic().to(device)
 crit_opt = torch.optim.Adam(crit.parameters(), lr=lr, betas=(beta_1, beta_2))
 
-## Initialise the Weights
+
+# Initialise the Weights
 def weights_init(m):
     if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
         torch.nn.init.normal_(m.weight, 0.0, 0.02)
     if isinstance(m, nn.BatchNorm2d):
         torch.nn.init.normal_(m.weight, 0.0, 0.02)
         torch.nn.init.constant_(m.bias, 0)
+
+
 gen = gen.apply(weights_init)
 crit = crit.apply(weights_init)
 
-## Create gradient
+
+# Create gradient
 def get_gradient(crit, real, fake, epsilon):
     mixed_images = real * epsilon + fake * (1 - epsilon)
     mixed_scores = crit(mixed_images)
     gradient = torch.autograd.grad(
         inputs=mixed_images,
         outputs=mixed_scores,
-        grad_outputs=torch.ones_like(mixed_scores), 
+        grad_outputs=torch.ones_like(mixed_scores),
         create_graph=True,
         retain_graph=True,
     )[0]
     return gradient
 
-## Define the Gradient Penalty
+
+# Define the Gradient Penalty
 def gradient_penalty(gradient):
     gradient = gradient.view(len(gradient), -1)
     gradient_norm = gradient.norm(2, dim=1)
-    penalty = torch.mean((gradient_norm - 1)**2)
+    penalty = torch.mean((gradient_norm - 1) ** 2)
     return penalty
 
-## Generator Loss
+
+# Generator Loss
 def get_gen_loss(crit_fake_pred):
     gen_loss = -1. * torch.mean(crit_fake_pred)
     return gen_loss
 
-## Critic Loss
+
+# Critic Loss
 def get_crit_loss(crit_fake_pred, crit_real_pred, gp, c_lambda):
     crit_loss = torch.mean(crit_fake_pred) - torch.mean(crit_real_pred) + c_lambda * gp
     return crit_loss
 
-## Define functions to Vizualise Images
+
+# Define functions to Visualise Images
 def show_tensor_images(image_tensor, num_images=25, size=(3, 64, 64)):
     image_tensor = (image_tensor + 1) / 2
     image_unflat = image_tensor.detach().cuda()
@@ -160,16 +174,18 @@ def show_tensor_images(image_tensor, num_images=25, size=(3, 64, 64)):
     plt.imshow(image_grid.permute(1, 2, 0).squeeze())
     plt.show()
 
+
 def make_grad_hook():
     grads = []
+
     def grad_hook(m):
         if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
             grads.append(m.weight.grad)
+
     return grads, grad_hook
 
-import matplotlib.pyplot as plt
 
-## Define the Training Loop and Start Training
+# Define the Training Loop and Start Training
 cur_step = 0
 generator_losses = []
 critic_losses = []
@@ -181,7 +197,7 @@ for epoch in range(n_epochs):
 
         mean_iteration_critic_loss = 0
         for _ in range(crit_repeats):
-            ### Update critic 
+            # Update critic
             crit_opt.zero_grad()
             fake_noise = get_noise(cur_batch_size, z_dim, device=device)
             fake = gen(fake_noise)
@@ -201,12 +217,12 @@ for epoch in range(n_epochs):
             crit_opt.step()
         critic_losses += [mean_iteration_critic_loss]
 
-        ### Update generator 
+        # Update generator
         gen_opt.zero_grad()
         fake_noise_2 = get_noise(cur_batch_size, z_dim, device=device)
         fake_2 = gen(fake_noise_2)
         crit_fake_pred = crit(fake_2)
-        
+
         gen_loss = get_gen_loss(crit_fake_pred)
         gen_loss.backward()
 
@@ -216,7 +232,7 @@ for epoch in range(n_epochs):
         # Keep track of the average generator loss
         generator_losses += [gen_loss.item()]
 
-        ### Visualization code 
+        # Visualization code
         if cur_step % display_step == 0 and cur_step > 0:
             gen_mean = sum(generator_losses[-display_step:]) / display_step
             crit_mean = sum(critic_losses[-display_step:]) / display_step
@@ -226,12 +242,12 @@ for epoch in range(n_epochs):
             step_bins = 20
             num_examples = (len(generator_losses) // step_bins) * step_bins
             plt.plot(
-                range(num_examples // step_bins), 
+                range(num_examples // step_bins),
                 torch.Tensor(generator_losses[:num_examples]).view(-1, step_bins).mean(1),
                 label="Generator Loss"
             )
             plt.plot(
-                range(num_examples // step_bins), 
+                range(num_examples // step_bins),
                 torch.Tensor(critic_losses[:num_examples]).view(-1, step_bins).mean(1),
                 label="Critic Loss"
             )
@@ -240,25 +256,26 @@ for epoch in range(n_epochs):
 
         cur_step += 1
 
-## Save the Generator and Critic (CUDA)
-torch.save(gen,'Generator.pt')
-torch.save(crit,'Critic.pt')
+# Save the Generator and Critic (CUDA)
+torch.save(gen, 'Generator.pt')
+torch.save(crit, 'Critic.pt')
 
-## Load the Generator and Generate Arts!!
+# Load the Generator and Generate Arts!!
 the_model = torch.load('Generator.pt')
+
 
 def denorm(img_tensors):
     return img_tensors * stats[1][0] + stats[0][0]
 
-from torchvision.utils import save_image
 
 sample_dir = '/Users/Sachith/GetArtsy'
 os.makedirs(sample_dir, exist_ok=True)
 
-def generateArts(n_arts=20):
+
+def generate_arts(n_arts=20):
     for art in range(n_arts):
         fake_images = the_model(get_noise(9, z_dim, device=device))
-        fake_fname = f'WGAN_Generated_{art}.png'
-        save_image(denorm(fake_images), os.path.join(sample_dir, fake_fname), nrow=3)
-        print('Saving', fake_fname)
+        fake_name = f'WGAN_Generated_{art}.png'
+        save_image(denorm(fake_images), os.path.join(sample_dir, fake_name), nrow=3)
+        print('Saving', fake_name)
         art = art + 1
